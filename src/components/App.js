952f4759da47547099f41bb2317257100f069f58
maybeworks-dev <vladislav1133light@ukr.net>
2018-08-23 11:05:17 +0300

    add select

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,30 @@ import React, { Component } from 'react'
 import PropTypes from'prop-types'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
+import Select from 'react-select';
 
 class App extends Component {
-    static propTypes = {
-
-    }
 
     state = {
-        username: ''
+        selection: null
     }
 
     render() {
+
+        const options = this.props.articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }))
+
         return (
             <div>
+                <Selec options = {options} value = {this.state.selection} onChange = {this.changeSelection}/>
                 <ArticleList articles = {this.props.articles}/>
             </div>
         )
     }
 
+    changeSelection  = selection => this.setState({selection})
 
 }
 
